766e06e9f1285dea9c913fd6858fd7b983c861ed
walterbaya <56239454+walterbaya@users.noreply.github.com>
2025-10-22 12:51:35 -0300

    se finaliza codigo de test-login.py

--- tests/test-login.py
+++ tests/test-login.py
@@ -1,20 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
 
 def test_login():
+
     driver = webdriver.Chrome()
+
+    driver.implicitly_wait(5)
 
     try:
         driver.get("https://www.saucedemo.com/")
 
-        time.sleep(1)
-
         driver.find_element(By.ID, "user-name").send_keys("standard_user")
         driver.find_element(By.ID, "password").send_keys("secret_sauce")
         driver.find_element(By.ID, "login-button").click()
-    
-        time.sleep(1)
 
         assert "/inventory" in driver.current_url, "No se redirigio correctamente al inventario."
 
@@ -26,13 +24,7 @@ def test_login():
 
         assert products_title == "Products"
 
-
-
-
-        print("Login exitoso y validado correctamente")
-
     except Exception as e:
-        print("Error en test_login: {e}")
         raise
     finally:
         driver.quit()
