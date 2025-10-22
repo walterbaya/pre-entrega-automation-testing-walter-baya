171778919e35db2393c8c915caffde9e454c6279
walterbaya <56239454+walterbaya@users.noreply.github.com>
2025-10-22 14:28:46 -0300

    se agrega verificacion y comprobacion inicial de elementos en test_inventory

--- tests/test_inventory.py
+++ tests/test_inventory.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from utils.login import login
 
 
 
@@ -17,11 +18,21 @@ def test_inventory():
         main_title = driver.find_element(By.CSS_SELECTOR, "div.app_logo").text 
 
         #Verificar que el título de la página de inventario sea correcto
-        assert main_title == "Swag Labs"
+        assert main_title == "Swag Labs", "Error, el titulo Swag Labs no esta presente"
 
         #Comprobar que existan productos visibles en la página (al menos verificar la presencia de uno)
 
+        products = driver.find_elements(By.CSS_SELECTOR, "div.inventory_item")
+
+        assert len(products) >= 1, "Error, no hay elementos en el inventario"
+
         #Lista nombre/precio del primero.
+
+        price = products[0].find_element(By.CSS_SELECTOR, "div.inventory_item_price").text
+        name =  products[0].find_element(By.CSS_SELECTOR, "div.inventory_item_name").text  
+
+        assert price == "$29.99"
+        assert name == "Sauce Labs Backpack"
 
         #Validar que elementos importantes de la interfaz estén presentes (menú, filtros, etc.)
 
