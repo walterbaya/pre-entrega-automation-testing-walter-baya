6b018bdd422e08a6f7030010d4af0ab750412e94
walterbaya <56239454+walterbaya@users.noreply.github.com>
2025-10-22 12:17:54 -0300

    se agregan validaciones de titulo == productos y de titulo principal == swag labs, verificando asi el ingreso a la pagina posterior al login, en el test_login.py

--- tests/test-login.py
+++ tests/test-login.py
@@ -14,9 +14,20 @@ def test_login():
         driver.find_element(By.ID, "password").send_keys("secret_sauce")
         driver.find_element(By.ID, "login-button").click()
     
-        time.sleep(4)
+        time.sleep(1)
 
         assert "/inventory" in driver.current_url, "No se redirigio correctamente al inventario."
+
+        main_title = driver.find_element(By.CSS_SELECTOR, "div.app_logo").text 
+
+        assert main_title == "Swag Labs"
+
+        products_title = driver.find_element(By.CSS_SELECTOR, "span.title").text 
+
+        assert products_title == "Products"
+
+
+
 
         print("Login exitoso y validado correctamente")
 
